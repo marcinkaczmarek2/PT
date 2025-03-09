883497aeb793dc60c3d74ec56b402e5f3e5214db
marcinkaczmarek2 <titnet112pl@gmail.com>
2025-03-09 14:50:32 +0100

    calculator with tested add and divide methods

---
+++ CalculationMachine/CalculationMachine/Calculator.cs
@@ -0,0 +1,18 @@
+﻿namespace CalculationMachine
+{
+    public class Calculator
+    {
+        public double Add(double a, double b) 
+        {
+            return a + b;
+        }
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Division by zero is forbidden");
+            }
+            return a / b;
+        }
+    }
+}
---
+++ CalculationMachine/CalculationMachineTest/CalculatorTest.cs
@@ -0,0 +1,45 @@
+﻿using CalculationMachine;
+
+namespace CalculationMachineTest
+{
+    [TestClass]
+    public sealed class CalculatorTest
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            Calculator calculator = new Calculator();
+
+            double a = 5d;
+            double b = 13d;
+
+            double result = calculator.Add(a, b);
+
+            Assert.AreEqual(result, 18);
+        }
+
+        [TestMethod]
+        public void TestDivide()
+        {
+            Calculator calculator = new Calculator();
+
+            double a = 26d;
+            double b = 13d;
+
+            double result = calculator.Divide(a, b);
+
+            Assert.AreEqual(result, 2);
+        }
+
+        [TestMethod]
+        public void TestDivideByZero()
+        {
+            Calculator calculator = new Calculator();
+
+            double a = 26d;
+            double b = 0d;
+
+            Assert.ThrowsException<DivideByZeroException>(() => calculator.Divide(a, b));
+        }
+    }
+}
