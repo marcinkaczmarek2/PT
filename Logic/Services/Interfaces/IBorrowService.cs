d97b2c171656a12f7c8150640bef21b4c1d31a5c
marcinkaczmarek2 <titnet112pl@gmail.com>
2025-05-19 14:34:49 +0200

    removed old logic layer

--- Logic/Services/Interfaces/IBorrowService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Logic.Services.Interfaces
-{
-    public interface IBorrowService
-    {
-        bool BorrowItem(Guid userId, Guid itemId);
-        bool ReturnItem(Guid userId, Guid itemId);
-    }
-
-}
